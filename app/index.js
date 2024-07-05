583d819b5fdde5c71fe0fcd6d517bd660bc5deae
Dhananjay320 <ahlawatdhananjay320@gmail.com>
2024-07-05 13:03:24 +0530

    commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React ,{useRef,useState, useEffect } from 'react';
+import { View, Text, StyleSheet,ActivityIndicator, } from 'react-native';
+
+export default function Index() {
+  return (
+     <View>
+      <Text>hello</Text>
+     </View>
+  );
+}
